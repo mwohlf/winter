2c70006f98b6a6bf62c54842a01b97a0686e5c3a
michael <michael@wohlfart.net>
2019-12-23 10:56:55 +0100

    cleanup the build scripts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 /*
  * see: https://docs.gradle.org/6.0.1/dsl/
  *      https://github.com/sdeleuze/spring-boot-kotlin-demo/blob/master/build.gradle.kts
@@ -11,5 +13,13 @@ repositories {
 
 plugins {
     java
+    kotlin("jvm") version "1.3.61"
 }
 
+tasks.register(name = "useMrPropper", type = DefaultTask::class) {
+    dependsOn("clean")
+    doFirst {
+        println("MrPropper cleaning, removing all artifacts")
+    }
+    delete(".gradle") // this doesn't work on windows i guess
+}
--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,10 +1,7 @@
 import com.moowork.gradle.node.NodeExtension
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-}
+import com.moowork.gradle.node.yarn.YarnTask
+import com.moowork.gradle.node.yarn.YarnInstallTask
+import com.palantir.gradle.docker.DockerExtension
 
 // see: https://github.com/hiper2d/spring-kotlin-angular-demo/blob/master/client/build.gradle.kts
 //      https://github.com/srs/gradle-node-plugin/issues/127
@@ -12,30 +9,38 @@ plugins {
     base
     // see: https://github.com/srs/gradle-node-plugin
     //      https://github.com/srs/gradle-node-plugin/blob/master/docs/node.md
+    //      https://github.com/hiper2d/spring-kotlin-angular-demo/blob/master/client/build.gradle.kts
     id("com.moowork.node") version "1.3.1"
-    // id("com.palantir.docker") version "0.22.1"
+    id("com.palantir.docker") version "0.22.1"
 }
 
 configure<NodeExtension> {
-    // see: https://stackoverflow.com/questions/58102283/maven-and-ivy-dependency-resolution-fails-with-gradle-6-0
     download = false // can't download atm because it looks for ivy.xml
+    // see: https://stackoverflow.com/questions/58102283/maven-and-ivy-dependency-resolution-fails-with-gradle-6-0
     distBaseUrl = "https://nodejs.org/dist"
     version = "13.5.0"
     npmVersion = "6.12.1"
     yarnVersion = "1.21.1"
 }
 
-tasks.register("greeting") {
-    doLast { println("Hello, World! project frontend") }
+
+tasks.register(name = "startLocal", type = YarnTask::class) {
+    dependsOn("yarn_install") // setting up the node_modules directory
+    // 'yarn start' is configured in package.json and executes 'ng serve'
+    args = listOf("start")
+    doFirst {
+        println("yarnStart running")
+    }
 }
 
-/*
-tasks.withType<Jar> {
-    dependsOn("yarn_run_build")
+tasks.register(name = "useMrPropper", type = DefaultTask::class) {
+    dependsOn("clean")
+    doFirst {
+        println("MrPropper cleaning, removing all artifacts")
+    }
+    delete("node_modules")
+    delete(".gradle") // this doesn't work on windows i guess
+    delete("yarn.lock")
 }
 
-val jar by tasks.getting(Jar::class) {
-    from("dist/todo-ui")
-    into("static")
-}
-*/
+
